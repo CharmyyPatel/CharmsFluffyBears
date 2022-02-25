1d43bac9a6c6de6f32875c631d8e0066de022e3e
CharmyyPatel <90396296+CharmyyPatel@users.noreply.github.com>
2022-02-25 12:34:11 -0400

    Successfully added an Controller.

---
+++ CharmsFluffyBears/Controllers/HelloWorldController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace CharmsFluffyBears.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome(string name, int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        }
+    }
+}
--- CharmsFluffyBears/ReadMe.txt
+++ CharmsFluffyBears/ReadMe.txt
@@ -1,4 +1,5 @@
 ﻿Date: 25th Feb 2022
 
 12:03 - I was almost done with the Project Assignment1 but it didn't reflect on the GITHUB. I was totally shattered but somehow collected myself and started whole project from very beginning.
-12.10 - Created the Project named "CharmsFluffyBears" i.e., a fictional name for my product which was "Teddy Bears."
+12:10 - Created the Project named "CharmsFluffyBears" i.e., a fictional name for my product which was "Teddy Bears."
+12:33 - Successfully added an Controller.
